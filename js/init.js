490a0c2fb47a5b1065781f5d5683275782bbf527
form43Adrian <81120457+form43Adrian@users.noreply.github.com>
2021-03-23 11:41:36 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,7 @@
+ apiKey: "AIzaSyBLufQ8AR7ndkfQjuOGIT86U9v40umTUpo",
+    authDomain: "autentifica43aqg.firebaseapp.com",
+    projectId: "autentifica43aqg",
+    storageBucket: "autentifica43aqg.appspot.com",
+    messagingSenderId: "979521072081",
+    appId: "1:979521072081:web:46a21ef4984c2dc23b0ef0",
+    measurementId: "G-3HERJK2HGH"

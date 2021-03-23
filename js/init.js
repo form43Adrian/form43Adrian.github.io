a0b1a3a05f1621a0031a0077b75ac4b7dc48577e
form43Adrian <81120457+form43Adrian@users.noreply.github.com>
2021-03-23 11:44:18 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,7 +1,12 @@
- apiKey: "AIzaSyBLufQ8AR7ndkfQjuOGIT86U9v40umTUpo",
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyBLufQ8AR7ndkfQjuOGIT86U9v40umTUpo",
     authDomain: "autentifica43aqg.firebaseapp.com",
     projectId: "autentifica43aqg",
     storageBucket: "autentifica43aqg.appspot.com",
     messagingSenderId: "979521072081",
     appId: "1:979521072081:web:46a21ef4984c2dc23b0ef0",
     measurementId: "G-3HERJK2HGH"
+    });
